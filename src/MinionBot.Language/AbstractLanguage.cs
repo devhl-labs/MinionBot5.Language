255be55811c457496142893570f32ddcd38b0dfd
devhl-labs <shawnkanyer@gmail.com>
2021-09-30 21:28:14 -0400

    added war pref help info

--- src/MinionBot.Language/AbstractLanguage.cs
+++ src/MinionBot.Language/AbstractLanguage.cs
@@ -46,6 +46,7 @@ namespace MinionBot.Languages
                 new CommandInfo(1, "setlanguage", Language.ManagementHelp.HelpSetLanguage, "languageName"),
                 new CommandInfo(1, "setprefix", Language.ManagementHelp.HelpSetPrefix, "yourPrefix"),
                 new CommandInfo(1, "deleteprefix", Language.ManagementHelp.HelpDeletePrefix),
+                new CommandInfo(1, "warpreferences", Language.ManagementHelp.HelpWarPreference),
 
 
                 //villages
